0ee67a0fd50123985674bb5c875ac26e6cf3d38f
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2022-06-21 06:22:12 -0500

    starting my adventure

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//Your Code here
+let repsonse = window.prompt("Do you head left or right?");
+console.log(repsonse);
+if(repsonse === 'left'){
+    //do something to coordinate with left//
+    console.log('do something to coordinate with left')
+} if else(repsonse === 'right'){
+    //do something to coordinate with right//
+    console.log('do something to coordinate with right');
+} else(){
+    //user ignored the the wishes of the council//
+    console.log('user ignored the the wishes of the council');
+}
